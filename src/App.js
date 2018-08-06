ac560675117668c23a8fabfb5606bd8501c29ce8
Daniel wawelska <daniel.drozdzel@gmail.com>
2018-08-06 16:35:46 +0200

    eslint react [WIP] Footer, header

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Daniel test </h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> ggggggasdasdasd and save to reload.
-        </p>
-      </div>
+    <div className='containter-fluid'>
+      <Header />
+      <Footer />  
+    </div>
     );
   }
 }
---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (<header className="row">
+    <div className="col"></div>
+    <div className="col-3">
+      <span>user@randomuser.pl</span>
+    </div>
+  </header>);
+};
+
+export default Header;
+
