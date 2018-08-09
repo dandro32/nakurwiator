a09ce04e7f25bbeb8f9c4dac50a90c1333a20719
Daniel wawelska <daniel.drozdzel@gmail.com>
2018-08-09 13:37:59 +0200

    navigation states changes test

--- src/components/UI/navigation/Navigation.test.js
+++ src/components/UI/navigation/Navigation.test.js
@@ -1,24 +1,38 @@
 import React from 'react';
 import Navigation from './Navigation';
 import ShallowRenderer from 'react-test-renderer/shallow';
+import TestRenderer from 'react-test-renderer';
 import { slide as Menu } from 'react-burger-menu';
 import menuNavigationConfigs from '../../../configs/menu-navigation.config';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Link } from 'react-router-dom';
+import { Link, HashRouter as Router } from 'react-router-dom';
 
 jest.mock('./NavigationRow', () => 'navigation row');
 
 describe('Navigation Component test', () => {
   let shallowRenderer;
   let shallowRendererResult;
-
+  let componentInstance;
   beforeEach(() => {
-
+    const root = TestRenderer.create(<Router><Navigation /></Router>).root;
+    componentInstance = root.findByType(Navigation).instance;
     shallowRenderer = new ShallowRenderer();
     shallowRenderer.render(<Navigation />);
     shallowRendererResult = shallowRenderer.getRenderOutput();
   });
 
+  it('should have isOpen initial state at false', () => {
+    expect(componentInstance.state.isOpen).toBe(false);
+  });
+
+  it('should have set isOpen initial state too false when onClick by CloseMenu method', () => {
+    const e = { preventDefault: jest.fn() };
+    componentInstance.state.isOpen = true;
+    expect(componentInstance.state.isOpen).toBe(true);
+    componentInstance.closeMenu(e);
+    expect(componentInstance.state.isOpen).toBe(false);
+  });
+  
   it('should rendred Menu component inside navigation', () => {
     expect(shallowRendererResult.type).toEqual(Menu);
   });
@@ -36,6 +50,4 @@ describe('Navigation Component test', () => {
       expect(link.props.children.type).toBe('navigation row');
     });
   });
-
-//TODO: test close menu and state isOpen
 });
