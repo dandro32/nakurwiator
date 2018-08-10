2b0362a604c34606e16c9d6aadbb356077f026cb
Daniel wawelska <daniel.drozdzel@gmail.com>
2018-08-10 12:47:46 +0200

    card images

--- src/components/shared/card/Card.js
+++ src/components/shared/card/Card.js
@@ -4,15 +4,18 @@ import PropTypes from 'prop-types';
 
 
 const Card = ({card}) => {
+  const imageSrc = process.env.PUBLIC_URL+card.imgSrc; // eslint-disable-line no-undef
   return (
-    <div className="card">
-      <img className="card-img-top" src={card.imgSrc} />
+    <div className="card"> 
+      <img className="card-img-top" src={imageSrc} alt={card.titile} /> 
       <div className="card-body">
         <h5 className="card-title">{card.title}</h5>
         <p className="card-text">{card.desc}</p>
-        <Link to={card.href} className="btn btn-primary">
-          {card.hrefTitle}
-        </Link>
+        <p className="text-center">
+          <Link to={card.href} className="btn btn-primary">
+            {card.hrefTitle}
+          </Link>
+        </p>
       </div>
     </div>
   );
--- src/components/trainings/Trainings.js
+++ src/components/trainings/Trainings.js
@@ -8,14 +8,14 @@ const Trainings = () => {
       desc: 'Zaplanuj sobie nowy trening i sprawdź pompę',
       href: '/trainigns/new',
       hrefTitle: 'Dawej',
-      imgSrc: 'obrazek'
+      imgSrc: '/img/training_new.jpg'
     },
     {
       title: 'Lista treningów',
       desc: 'Obczaj swoje treningi, edytuj lub usuwaj',
       href: '/trainigns/list',
       hrefTitle: 'Sprawdzam',
-      imgSrc: 'obrazek2'
+      imgSrc: '/img/training_list.jpg'
     }
   ];
   return (<Dashboard cards={cards} />);
