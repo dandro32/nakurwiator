56cbd860235034a2b8ea351b99ab971793057e06
Daniel wawelska <daniel.drozdzel@gmail.com>
2018-08-16 18:27:50 +0200

    [WIP] React table api.config

---
+++ src/components/UI/table/Table-wrapper.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class TableWrapper extends Component {
+  render() {
+    return <p>Testsss</p>;
+  }
+
+}
+
+export default TableWrapper;
---
+++ src/components/trainings/trainings-list/Trainings-list.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class TrainingsList extends Component {
+  render() {  
+    return <p>Testsss</p>;
+  }
+
+}
+
+export default TrainingsList;
